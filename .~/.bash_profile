5730c53406cd9b89681a512218dc2c180be3bc61
albarto2021 <81423967+albarto2021@users.noreply.github.com>
2021-11-08 23:42:36 +0300

    first commit

---
+++ .~/.bash_profile
@@ -0,0 +1,10 @@
+
+
+export PATH
+
+export ANDROID_SDK=/Users/asus/AppData/Local/Android/Sdk
+
+[ -d "$HOME/Users/asus/AppData/Local/Android/Sdk" ] && ANDROID_SDK=$HOME/Users/asus/AppData/Local/Android/Sdk || ANDROID_SDK=$HOME/Android/Sdk
+echo "export ANDROID_SDK=$ANDROID_SDK" >> ~/`[[ $SHELL == *"zsh" ]] && echo '.zshenv' || echo '.bash_profile'`
+
+echo "export PATH=$HOME/Users/asus/AppData/Local/Android/Sdk/platform-tools:\$PATH" >> ~/`[[ $SHELL == *"zsh" ]] && echo '.zshenv' || echo '.bash_profile'`
--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
+// View -> UIView
 export default function App() {
+  let x = 1;
+  console.log("App executed");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, My First React Native project</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
